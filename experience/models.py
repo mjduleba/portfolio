e50b3a6b9dbb6649d795c299b6328d7a53f30c64
Michael Duleba <michael.duleba1@gmail.com>
2026-07-02 09:08:05 -0400

    Added location field to experience model

--- experience/models.py
+++ experience/models.py
@@ -5,6 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 class Experience(models.Model):
     role = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     bullets = ArrayField(models.CharField(max_length=200), default=list)
