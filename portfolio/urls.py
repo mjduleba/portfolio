644bdef6497f288183a59a71a86a05dc980b3cd1
Michael Duleba <michael.duleba1@gmail.com>
2026-07-03 19:11:38 -0400

    Added all URLs to Portfolio project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
+# URL patterns for porfolio backend
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
+    path('api/', include('experience.urls')),
+    path('api/', include('skills.urls')),
+    path('api/', include('userprofile.urls')),
 ]
