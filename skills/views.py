a8eb611f7268447b16c34433bf391c4817a3fb60
Michael Duleba <michael.duleba1@gmail.com>
2026-07-03 19:10:46 -0400

    Created files for endpoint requests for Skills model

--- skills/views.py
+++ skills/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Skill
+from .serializers import SkillSerializer
 
-# Create your views here.
+class SkillViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
