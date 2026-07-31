d02dc4fa9d029eaa075082dcbdd1a370acd04ae8
Michael Duleba <michael.duleba1@gmail.com>
2026-07-31 08:35:18 -0400

    Inserted skills section into main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,16 @@
 import About from "@/components/sections/About";
+import Skills from "@/components/sections/Skills";
 import { getProfile } from "@/lib/api/profile";
+import { getSkills } from "@/lib/api/skills";
 
 export default async function Home() {
-  const profile = await getProfile();
+  const [profile, skills] = await Promise.all([getProfile(), getSkills()]);
 
   return (
     <main className="flex flex-1 flex-col">
       <section id="intro" />
       <About profile={profile} />
+      <Skills skills={skills} />
     </main>
   );
 }
